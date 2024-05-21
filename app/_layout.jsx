27e87b6e6a47d887c5173bc291b4b9bb61144db1
kareemsnn <161267576+kareemsnn@users.noreply.github.com>
2024-05-21 17:59:22 -0400

    Edit profile screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,7 @@ import {Slot, Stack} from 'expo-router'
 const RootLayout = () => {
   return (
     <Stack>
-        <Stack.Screen name= "index" options = {{ headerShown: false}} />
+        <Stack.Screen name= "index" options = {{headerShown: false}} />
     </Stack>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className ="text-3xl">The Masjid App</Text>
       <StatusBar style="auto" />
-      <Link href ="/profile" style= {{color:'blue'}}>Got to Profile</Link>
+      <Link href ="/Profile" style= {{color:'blue'}}>Got to Profile</Link>
     </View>
   );
 }
