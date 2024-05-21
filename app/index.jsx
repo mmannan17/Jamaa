55f3f86d4d62cd83b16190d81f523ae11436cecc
kareemsnn <161267576+kareemsnn@users.noreply.github.com>
2024-05-20 21:16:56 -0400

    Base App

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {Link} from 'expo-router'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className ="text-3xl">The Masjid App</Text>
       <StatusBar style="auto" />
+      <Link href ="/profile" style= {{color:'blue'}}>Got to Profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
