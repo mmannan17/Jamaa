5a8c0e79738d7d9e02ee3dbf61cbbc5e0c4186dd
kareemsnn <161267576+kareemsnn@users.noreply.github.com>
2024-05-26 22:19:29 -0400

    Created Sign up page

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, Text, View, Image } from 'react-native';
-import {Link} from 'expo-router';
+import {Redirect, router} from 'expo-router';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
     <SafeAreaView className = 'bg-primary h-full'>
       <ScrollView contentContainerStyle = {{height: '100%'}}>
-        <View className =" w-full  items-center h-full px-4">
+        <View className =" w-full  items-center min-h-[85vh] px-4">
           <Image
             source = {images.logo}
             className = "w-[130px] h-[84px]"
@@ -20,14 +21,22 @@ export default function App() {
           resizeMode='contain'
           />
           <View className = "relative mt-5">
-            <Text className = "text-3xl text-white font-bold text-center"> Welcome to 
+            <Text className = "text-3xl text-white font-bold text-center"> Welcome to
             <Text className = "text-secondary-200"> The Masjid App{"\n"}</Text>
             </Text>
-
           </View>
+
+          <Text className="text-sm font-pregular text-white mt-7 text-center mb-5">Where you can find all the masjids around you 
+          and find events nearby</Text>
+          
+          <CustomButton
+          title = "Sign Up/Sign In"
+          handlePress ={() => router.push('/sign-in')} 
+          containerStyles="w-full mt-7"/>
         </View>
       </ScrollView>
-    </SafeAreaView>
-  );
+      <StatusBar backgroundColor='#161622' style='light'/>
+    </SafeAreaView>  
+    );
 }
 
