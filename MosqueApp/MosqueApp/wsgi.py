2421ee6210e2b727381177d0a19a198e1b8d9047
Mustafa Mannan <mannan2@usf.edu>
2024-07-01 00:36:54 -0400

    pls work

--- MosqueApp/MosqueApp/wsgi.py
+++ MosqueApp/MosqueApp/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
 
+
+import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MosqueApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MosqueApp.MosqueApp.settings')
 
 application = get_wsgi_application()
---
+++ applications.py
@@ -0,0 +1,1 @@
+from MosqueApp.MosqueApp.wsgi import application
